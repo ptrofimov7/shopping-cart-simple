2f08a4f9e1d076501c8313a5bfdd35d87433d9f4
Peter Trofimov <ptrofimov7@gmail.com>
2023-03-16 10:19:22 +0200

    add redirect to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import { store } from './app/store'
 import { sagaActions } from "./features/shopping-cart/sagaActions";
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom"
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 
@@ -14,6 +14,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
         </Routes>
       </Provider>
     </BrowserRouter>
